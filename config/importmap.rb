f0cf30c2b7477cf3b2ed7e85d7d9dc88d7bc410d
erniebondy <erniebondy@hotmail.com>
2025-09-16 01:16:22 -0700

    add bootstrap css/js. pin importmap. add navbar & style

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,11 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+
+### pin "name_used_in_import", to: "path_to_file"
+pin "bootstrap", to: "bootstrap.bundle.min.js"
+
+
+### And remember, it was soo difficult downloading the file from
+### the official website and adding it to the folder
+### and then having to 'pin' it so that the import works
