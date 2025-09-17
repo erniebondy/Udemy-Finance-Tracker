d07e7009a1be3337d5e5f30edc301f8260b9aa48
erniebondy <erniebondy@hotmail.com>
2025-09-17 12:48:49 -0700

    fix flash messages. add 'alphavantagerb' gem for stock info

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_15_203313) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_16_222714) do
   create_table "users", force: :cascade do |t|
-    t.string "username", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
